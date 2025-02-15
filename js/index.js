fc5eccc893ce68f2a2d068a071d7594b143ff515
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-15 21:59:27 +0300

    Deletion event added

--- js/index.js
+++ js/index.js
@@ -1,39 +1,8 @@
 // Todo listesi
-const todoList = [];
+let todoList = [];
 
 const todoForm = document.querySelector("#todoForm");
 console.log(todoForm);
-
-const listRender = () => {
-  document.querySelector("#todoList").innerHTML = ""; //
-  todoList.forEach((todo, index) => {
-    console.log(index, todo);
-    const todoItem = document.createElement("li"); // <li></li>
-    todoItem.classList.add("listItem"); // <li class="listItem"></li>
-    todoItem.innerHTML = `
-       ${todo.message}
-                <div style="display: flex; gap: 20px">
-                  <i class="icon fi fi-rr-check"></i>
-                  <i class="icon fi fi-rr-trash" id="${index}"></i>
-                </div>
-      `; // <li class="listItem">
-      /* // Mürsel 
-       <div style="display: flex; gap: 20px">
-                  <i class="icon fi fi-rr-check"></i>
-                  <i class="icon fi fi-rr-trash" id="${index}"></i>
-                </div>
-                </li> */
-    document.querySelector("#todoList").appendChild(todoItem);
-  });
-  /* 
-    <li class="listItem">
-                Mürsel
-                <div style="display: flex; gap: 20px">
-                  <i class="icon fi fi-rr-check"></i>
-                  <i class="icon fi fi-rr-trash"></i>
-                </div>
-              </li> */
-};
 
 todoForm.addEventListener("submit", (event) => {
   event.preventDefault(); // Sayfanın yenilenmesini engeller.
@@ -48,3 +17,28 @@ todoForm.addEventListener("submit", (event) => {
 
   todoForm.reset();
 });
+
+const listRender = () => {
+  console.log("Todo List:", todoList);
+  document.querySelector("#todoList").innerHTML = ""; //
+  todoList.forEach((todo, todoIndex) => {
+    const todoItem = document.createElement("li"); // <li></li>
+    todoItem.classList.add("listItem"); // <li class="listItem"></li>
+    todoItem.innerHTML = `
+       ${todo.message}
+                <div style="display: flex; gap: 20px">
+                  <i class="icon fi fi-rr-trash" id="todo-${todoIndex}"></i>
+                </div>
+      `;
+
+    document.querySelector("#todoList").appendChild(todoItem);
+    document
+      .querySelector(`#todo-${todoIndex}`)
+      .addEventListener("click", (event) => {
+        todoList = todoList.filter((item, index) => {
+          return index !== Number(event.target.id.split("-")[1]);
+        });
+        listRender();
+      });
+  });
+};
